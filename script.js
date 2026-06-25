99becdd9f4703d894d8d37e04791f7a42d5a6e2b
eloabarusso-art <eloa.barusso@escola.pr.gov.br>
2026-06-25 17:09:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+let tamanho = 16;
 
+function aumentarFonte(){
+    tamanho += 2;
+    document.body.style.fontSize = tamanho + "px";
+}
+
+function diminuirFonte(){
+    tamanho -= 2;
+    document.body.style.fontSize = tamanho + "px";
+}
+
+function altoContraste(){
+    document.body.classList.toggle("contraste");
+}
+
+function resposta(tipo){
+
+    if(tipo === "certo"){
+        document.getElementById("resultado").innerHTML =
+        "✅ Parabéns! Energia solar é renovável.";
+    }else{
+        document.getElementById("resultado").innerHTML =
+        "❌ Resposta incorreta. Tente novamente!";
+    }
+
+}
+
+let arvores = 0;
+
+function plantarArvore(){
+    arvores++;
+    document.getElementById("contador").innerHTML = arvores;
+}
+
+function tocarSom(){
+    document.getElementById("som").play();
+}
